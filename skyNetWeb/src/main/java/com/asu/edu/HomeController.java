5007b0c730fe922658ee9e986f2a185abd65eaeb
amitnain <anain@asu.edu>
2012-11-02 07:51:49 -0700

    Admin log files UI

--- skyNetWeb/src/main/java/com/asu/edu/HomeController.java
+++ skyNetWeb/src/main/java/com/asu/edu/HomeController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -57,5 +58,39 @@ public class HomeController {
 		logger.info("Admin Modify user request");
 		// Modify user
 		return "redirect:/admin";
-	}	
+	}
+	@RequestMapping(value = "/admin-regularEmp", method = RequestMethod.GET)
+	public String admin_regularEmp(Locale locale, Map model, Model attr) {
+		logger.info("Admin screen");
+		attr.addAttribute("role_id","regEmp");
+		model.put("users", adminDAO.getUsersByRole(3));
+		return "admin-user";
+	}
+	@RequestMapping(value = "/admin-deptMgr", method = RequestMethod.GET)
+	public String admin_deptMgr(Locale locale, Map model, Model attr) {
+		logger.info("Admin screen");
+		attr.addAttribute("role_id","deptMgr");
+		model.put("users", adminDAO.getUsersByRole(2));
+		return "admin-user";
+	}
+	@RequestMapping(value = "/admin-copMgr", method = RequestMethod.GET)
+	public String admin_corporateMgr(Locale locale, Map model, Model attr) {
+		logger.info("Admin screen");
+		attr.addAttribute("role_id","copMgr");
+		model.put("users", adminDAO.getUsersByRole(4));
+		return "admin-user";
+	}
+	@RequestMapping(value = "/admin-guest", method = RequestMethod.GET)
+	public String admin_guestUser(Locale locale, Map model, Model attr) {
+		logger.info("Admin screen");
+		attr.addAttribute("role_id","guestUsr");
+		model.put("users", adminDAO.getUsersByRole(5));
+		return "admin-user";
+	}
+	@RequestMapping(value = "/admin-logs", method = RequestMethod.GET)
+	public String admin_logs(Locale locale, Map model) {
+		logger.info("Admin screen");
+		model.put("logfiles", adminDAO.getLogFiles());
+		return "admin-systemlog";
+	}
 }
