90f42eb5c816440316b2d33c5231b9f44f4d719c
amitnain <anain@asu.edu>
2012-11-02 07:48:25 -0700

    Admin log files UI

---
+++ skyNetWeb/src/main/java/com/asu/edu/base/vo/LogFilesVO.java
@@ -0,0 +1,19 @@
+package com.asu.edu.base.vo;
+
+public class LogFilesVO {
+	private String pathName;
+	private String modifiedDate;
+	
+	public String getPathName() {
+		return pathName;
+	}
+	public void setPathName(String pathName) {
+		this.pathName = pathName;
+	}
+	public void setModifiedDate(String modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+	public String getModifiedDate() {
+		return modifiedDate;
+	}
+}
