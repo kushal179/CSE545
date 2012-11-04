acb7987a45160ae0b859401780facf2c4f28c80e
kbandi <kushal.bandi@intel.com>
2012-11-03 22:51:06 -0700

    File VO

---
+++ skyNetWeb/src/main/java/com/asu/edu/base/vo/FileVO.java
@@ -0,0 +1,87 @@
+package com.asu.edu.base.vo;
+
+public class FileVO {
+	
+	private int id;
+	private String fileName;
+	private String type;
+	private String path;
+	private int ownerId;
+	private int deptId;
+	private String modTime;
+	private String createTime;
+	private int lock;
+	private String password;
+	private int parentId;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getModTime() {
+		return modTime;
+	}
+	public void setModTime(String modTime) {
+		this.modTime = modTime;
+	}
+	public String getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+	public int getLock() {
+		return lock;
+	}
+	public void setLock(int lock) {
+		this.lock = lock;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getOwnerId() {
+		return ownerId;
+	}
+	public void setOwnerId(int ownerId) {
+		this.ownerId = ownerId;
+	}
+	public int getDeptId() {
+		return deptId;
+	}
+	public void setDeptId(int deptId) {
+		this.deptId = deptId;
+	}
+	public int getParentId() {
+		return parentId;
+	}
+	public void setParentId(int parentId) {
+		this.parentId = parentId;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	
+	
+	
+
+}
